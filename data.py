f273e212ef0b7852978ec5d08caf53a134faffb8
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-09-24 10:47:51 +0300

    data: add image tensor transformation

--- data.py
+++ data.py
@@ -1,15 +1,23 @@
+import torch
 from torchvision import datasets
+from torchvision import v2
 
+transform = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32,scale=True)
+])
 train_dataset = datasets.MNIST(
     root="data",
     train = True,
-    download=True
+    download=True,
+    transform=transform
 )
 
 test_dataset = datasets.MNIST(
     root="data",
     train=False,
-    download=True
+    download=True,
+    transform=transform
 )
 
 print("Training dataset Length:",len(train_dataset))
@@ -19,3 +27,6 @@ print(image)
 print("Image type:",type(image))
 print("Image size:", image.size)
 print("Label:",label)
+
+
+image.show()
